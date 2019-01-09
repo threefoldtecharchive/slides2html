9f4c6e1d548f3bcb2584861308cbbdbcc0cc01a1
xmonader <xmonader@gmail.com>
2019-01-09 19:50:43 +0200

    auto install requirements using install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ except ImportError:
     # can't have the entry_points option here.
     from distutils.core import setup
 
+
+required = []
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='slides2html',
       version='1.0.0',
       author="Ahmed T. Youssef",
@@ -14,6 +19,7 @@ setup(name='slides2html',
       scripts=['scripts/slides2html'],
       url="https://github.com/threefoldtech/slides2html",
       license='BSD 3-Clause License',
+      install_requires=required,
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
