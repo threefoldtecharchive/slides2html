4b7af6c072dcfc55a4f030868aa10d929f4f275c
xmonader <xmonader@gmail.com>
2019-03-19 10:39:00 +0200

    add resizing images utils

--- slides2html/image_utils.py
+++ slides2html/image_utils.py
@@ -4,7 +4,47 @@ from concurrent.futures import ThreadPoolExecutor, wait
 from PIL import Image
 
 
+def resize_image(path, newsize):
+    """resize image to a new size
+
+    Arguments:
+        path {str} -- Image path
+
+    Keyword Arguments:
+        newsize {tuple} -- new size (width,height)
+    """
+    img = Image.open(path)
+    img.thumbnail(newsize)
+    img.save(path)
+
+
+def resize_images(destdir, newsize):
+    """resize batch of images in destdir to a new size 
+    
+    Arguments:
+        destdir {str} -- directory of google slides exported images
+        newsize {tuple} -- tuple of width, height of the new size
+    """
+
+    files = [x for x in os.listdir(destdir) if "png" in x and "_" in x and "background_" not in x]
+    files.sort(key=lambda k: int(k.split("_")[0]))
+    results = []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for f in files:
+            fullpath = os.path.join(destdir, f)
+            future = executor.submit(resize_image, fullpath, newsize)
+            results.append(future)
+    wait(results)
+
+
 def to_transparent_background_image(path, newpath):
+    """convert image to image with transparent background
+    
+    Arguments:
+        path {str} -- old image
+        newpath {str} -- new image
+    """
+
     img = Image.open(path)
     img = img.convert("RGBA")
 
@@ -20,6 +60,13 @@ def to_transparent_background_image(path, newpath):
 
 
 def layer_image(foregroundimg, backgroundimg):
+    """layer foregroundimg on top of backgroundimg
+    
+    Arguments:
+        foregroundimg {str} -- path of foreground image
+        backgroundimg {str} -- path of background image
+    """
+
     background = Image.open(backgroundimg)
     foreground = Image.open(foregroundimg)
 
@@ -28,6 +75,12 @@ def layer_image(foregroundimg, backgroundimg):
 
 
 def images_to_transparent_background(destdir):
+    """convert batch of images to transparent background images
+    
+    Arguments:
+        destdir {str} -- path of a directory with google slides exported images
+    """
+
     files = [x for x in os.listdir(destdir) if "png" in x and "_" in x and "background_" not in x]
     files.sort(key=lambda k: int(k.split("_")[0]))
     results = []
@@ -40,6 +93,13 @@ def images_to_transparent_background(destdir):
 
 
 def set_background_for_images(destdir, bgpath):
+    """Apply background to all images in destdir
+    
+    Arguments:
+        destdir {str} -- directory with exported google slides as images
+        bgpath {str} -- background path
+    """
+
     files = [x for x in os.listdir(destdir) if x.endswith("png") and "_" in x and "background_" not in x]
     files.sort(key=lambda k: int(k.split("_")[0]))
     results = []
